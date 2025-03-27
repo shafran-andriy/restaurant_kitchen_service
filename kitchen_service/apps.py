c4dc86b30c512bfc55f4438a390927fe629ec9a4
shafran <shafran.andriy@gmail.com>
2025-03-27 13:16:39 +0200

    Add models to models.py & reformat files using black

--- kitchen_service/apps.py
+++ kitchen_service/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class KitchenServiceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'kitchen_service'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "kitchen_service"
