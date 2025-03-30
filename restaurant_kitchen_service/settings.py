3a97224c15335409ed6ef7eca32a83a037dc33f4
shafran <shafran.andriy@gmail.com>
2025-03-30 14:14:55 +0300

    fix: Moved SECRET_KEY from restaurant_kitchen_service/settings.py to file .env in the venv

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ("django-insecure"
-              "-5&^in(meqq_%6pn(lw=&fms_!^hqjdo&guu62qyclg2rj^hnv@")
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key-for-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
